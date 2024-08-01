fff9c0c3661390c7846f00ac0277d11e8b74e95c
Winnie-Flora <contentcreatorwinnie@gmail.com>
2024-08-01 22:26:19 +0300

    All changes on props

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home name = {user.name} city = {user.city}/>
+      <Home color = {user.color} name = {user.name} city = {user.city}/>
       <About bio = {user.bio} github = {user.links.github} linkedin = {user.links.linkedin}/>
     </div>
   );
